0f422283f15cb6064b90074158bc69623930b074
mionki <piousmionki@gmail.com>
2022-03-11 01:20:43 +0300

    third commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * prints the phrase with proper grammar,
+ * but the outcome is a piece of art,
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
