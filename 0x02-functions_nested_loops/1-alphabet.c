45be8bc867a6054706660806706edcfac1339363
mionki <piousmionki@gmail.com>
2022-03-15 21:23:01 +0300

    not

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *print_alpahabet- prints the alphabets in lowercase
+ */
+void print_alphabet(void)
+{
+	char al;
+
+	al = 'a';
+
+	while (al <= 'z')
+	{
+		_putchar(al);
+		al++;
+	}
+
+	_putchar('\n');
+}	
