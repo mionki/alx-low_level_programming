e0252ee0fbb4a41785ff9eb8ae2973327e2d245a
mionki <piousmionki@gmail.com>
2022-03-15 21:05:22 +0300

    put

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,19 @@
-#include<stdio.h>
-/*main prints putchar
- *retun value is 0 */
+#include"main.h"
+/**
+ *main prints putchar
+ *return value is 0
+ */
 
-int main() {
-printf("_putchar /n");
+int main(void) {
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+		
 return 0;
 }
