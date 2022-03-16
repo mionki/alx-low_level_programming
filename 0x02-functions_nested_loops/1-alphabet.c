510be39abb6235389e4020acdf64d143d319b778
mionki <piousmionki@gmail.com>
2022-03-16 16:30:17 +0300

    task

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,9 @@
 #include "main.h"
 
 /**
- *main- contains print_alpahabet that prints the alphabets in lowercase
- *return: Value is 0 when program successful
+ *print_alpahabet - prints the alphabets in lowercase
  */
-int main(void)
-{	
+	
 void print_alphabet(void)
 {
 	char al;
@@ -20,5 +18,5 @@ void print_alphabet(void)
 
 	_putchar('\n');
 }	
-return (0);
-}
+
+
