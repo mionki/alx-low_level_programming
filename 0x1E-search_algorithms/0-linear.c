9ea644867ac58401e060fd592116341152714816
mionki <piousmionki@gmail.com>
2022-10-05 17:13:58 +0300

    wronng

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -7,9 +6,9 @@
 /**
  * linear_search - searches for a value in an array with linear search algo
  * 
- * @param array: pointer to the first element of the array 
- * @param size : number of elements in the array
- * @param value : value to search
+ * array: pointer to the first element of the array 
+ * size : number of elements in the array
+ * value : value to search
  * 
  * Return: return index of the value in the array or -1. 
  */
