893e4991168d11b82f3c94f497a6a80483fbfa8c
mionki <piousmionki@gmail.com>
2022-10-05 17:10:29 +0300

    correction 1

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -16,25 +16,19 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-    for ( i = 0; i <= size; i++)
-    {
-        if (array[i] == value)
-        {
-            printf("%d",array[i]);
-            return i;
-        }
-        else if (array[i] != value)
-        {
-            printf("%d",array[i]);
-        }
-        else
-        {
-            return (-1);
-            
-        }
-          
-        
-    }
+    size_t i = 0;
 
+	if (!array || size == 0)
+		return (-1);
+
+	while (i < size)
+	{
+		printf("[%d]", array[i]);
+		if (array[i] == value)
+			return (i);
+		i++;
+	}
+
+	return (-1);
 
 }
